82dee4ef88cc93c463c3142fbfa7e2aa1930bb30
ofoscar <ktaoscar@gmail.com>
2025-07-05 21:36:32 -0700

    feat: reorder imports in Contact component for consistency

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import GlassButton from './GlassButton';
 import Footer from './Footer';
+import GlassButton from './GlassButton';
 
 interface FormData {
   name: string;
